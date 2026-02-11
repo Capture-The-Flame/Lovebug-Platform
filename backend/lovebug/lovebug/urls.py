029ff3be5811f30a93a9b66f2c8ba69dea059744
Anirudh Yallapragada <110635407+anirudh-56@users.noreply.github.com>
2026-02-10 19:50:16 -0600

    Add test endpoint

--- backend/lovebug/lovebug/urls.py
+++ backend/lovebug/lovebug/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from login import views as login_views
+from login.test_views import test_site
+
 
 urlpatterns = [
     path("bobferguson/", admin.site.urls),
@@ -8,5 +10,6 @@ urlpatterns = [
     path("api/me/", login_views.me, name="me"),
     path("api/logout/", login_views.api_logout, name="api_logout"),
     path("", login_views.index, name="index"),
+     path('test-site/', test_site),
     path("", include("login.urls")) 
 ]
